561636662cac9d0bd936dbbff102c46f22ef3549
mhdix <domanloom@gmail.com>
2025-02-02 00:26:48 -0800

    feature: complate addTodo to json-server

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addAsyncTodo } from "../features/todo/todoSlice";
 
 const TodoAdd = () => {
-  return (
-    <div>TodoAdd</div>
-  )
-}
+  const [addTodo, setAddTodo] = useState("");
+  const { loading, error } = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
 
-export default TodoAdd
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addAsyncTodo(addTodo));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {loading ? <p>loading ... </p> : error ? <p>{error}</p> : null}
+        <input type="text" onChange={(e) => setAddTodo(e.target.value)} />
+        <button>submit form</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoAdd;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -23,6 +23,19 @@ const getAsyncTodo = createAsyncThunk(
   }
 );
 
+const addAsyncTodo = createAsyncThunk(
+  "addTodo/addAsyncTodo",
+  async (todoData, { rejectWithValue }) => {
+    console.log(todoData)
+    try {
+      const response = await api.post("/todos", {text: todoData, complated: false});
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const todoSlice = createSlice({
   name: "todo",
   initialState,
@@ -43,9 +56,24 @@ const todoSlice = createSlice({
         state.loading = false;
         state.error = null;
         state.todo = action.payload;
+      })
+      .addCase(addAsyncTodo.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+        state.todo = [];
+      })
+      .addCase(addAsyncTodo.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = false;
+        state.todo.push(action.payload);
+      })
+      .addCase(addAsyncTodo.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.todo = [];
       });
   },
 });
 
-export { getAsyncTodo };
+export { getAsyncTodo, addAsyncTodo };
 export default todoSlice.reducer;
